ecbde64e76c4bc45d5a819411586ec2b50f9eaaa
Arnold Lee <arnoldlee60@gmail.com>
2022-03-27 01:10:07 -0500

    Front end stuff

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,119 @@
+const router = require('express').Router();
+const { User, Post } = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+      order: [['created_at', 'DESC']],
+      include: [
+        {
+          model: User,
+          attributes: ['email']
+        },
+      ]
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+      });
+  });
+
+router.post('/', withAuth, (req, res) => { //createForm
+    Post.create({
+      title: req.body.title,
+      post_content: req.body.post_content,
+      user_id: req.session.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+//post/1
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'title',
+        'created_at',
+        'post_content'
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['email']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'Error' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+
+
+// post/1
+router.put('/:id', withAuth, (req, res) => {
+    Post.update({
+        title: req.body.title,
+        post_content: req.body.post_content
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'Error' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+
+  //delete
+  router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'Error' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,19 @@
+const User = require('./User');
+const Post = require('./Post');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+  
+//======================
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+  
+
+
+
+module.exports = { User, Post }
+// module.exports = { Post }
+// module.exports = { Comment }
